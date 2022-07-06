b0915efc4a2fb4e080400409a5102622208dea28
Marwan Nour <marwan.nour@polytechnique.edu>
2022-07-06 16:55:25 +0200

    Added jq parsing, http auth and insecure curls

--- README.md
+++ README.md
@@ -12,5 +12,42 @@ Generated with [Bashly](https://github.com/DannyBen/bashly).
 ## Usage
 
 ```bash
+./regharvest --help
+regharvest - Harvest Blobs from Docker Registry V2
 
+Usage:
+  regharvest SOURCE OUTPUT [options]
+  regharvest --help | -h
+  regharvest --version | -v
+
+Options:
+  --help, -h
+    Show this help
+
+  --version, -v
+    Show version number
+
+  --user, -u USER
+    Username to use for logging in
+
+  --pass, -p PASSWORD
+    Password to use for logging in
+
+  --insecure, -k
+    Allow insecure server connections when using SSL
+
+  --debug, -d
+    Print Debug output
+
+Arguments:
+  SOURCE
+    URL (or IP) to download from
+
+  OUTPUT
+    Output folder
+
+Examples:
+  regharvest example.com output-folder
+  regharvest example.com output-folder --user username --pass password
+  regharvest example.com output-folder --insecure
 ```
--- src/root_command.sh
+++ src/root_command.sh
@@ -16,10 +16,9 @@ echo "/_/ /_/\__,_/_/    |___/\___/____/\__/\___/_/     "
 echo ""
 
 # Debug mode
-debug=0
-if [[ (! -z "${args[--debug]}") ||  (! -z "${args[-d]}") ]]; then
-  set -x 
-  debug=1
+if [[ ${args[--debug]} -eq 1 ]]; then
+  inspect_args
+  set -x
 fi
 
 # Check jq requirement
@@ -30,12 +29,22 @@ if [[ $? -eq 127 ]]; then
   exit 1
 fi
 
-
-
-
 echo ""
 
-response=$(curl --silent  ${args[source]})
+# Insecure
+insecure=""
+if [[ ${args[--insecure]} -eq 1 ]]; then
+  insecure="-k"
+fi
+
+# Auth (user, pass)
+auth_str=""
+if [[ ! -z ${args[--user]} ]] && [[ ! -z ${args[--pass]} ]]; then
+  auth_str="--user ${args[--user]}:${args[--pass]}"
+fi
+
+# Curl catalog
+response=$(curl --silent $insecure $auth_str ${args[source]}/v2/_catalog)
 
 if [[ $? -ne 0 ]]; then
   echo "Error sending request to registry"
@@ -43,7 +52,48 @@ if [[ $? -ne 0 ]]; then
   exit 1
 fi
 
-if [[ $debug -eq 1 ]]; then
-  printf "\n$response\n"
-fi
+# Parse catalog
+repositories=$(echo $response | jq -r .repositories[])
+printf "\n\nAvailable Repositories:\n\n$repositories\n\n"
 
+read -p "Choose a repository from the items above: " repository 
+
+# Loop over repositories
+for repo in $repositories; do
+  # Check if input is correct
+  if [[ $repository == $repo ]]; then
+    # Get tags
+    response=$(curl --silent $insecure $auth_str ${args[source]}/v2/$repo/tags/list)
+    
+    # Parse tags
+    tags=$(echo $response | jq -r .tags[])
+    printf "\n\nAvailable Tags:\n\n$tags\n\n"
+
+    read -p "Choose a tag from the items above: " tag 
+
+    # Loop over tags
+    for t in $tags; do
+      # Check if input is correct
+      if [[ $tag == $t ]]; then
+        # Get manifest
+        response=$(curl --silent $insecure $auth_str ${args[source]}/v2/$repo/manifests/$tag)
+        blobs=$(echo $response | jq -r .fsLayers[].blobSum)
+        echo $blobs
+
+        # TODO: Save blobs in a set (no duplicates)
+        # TODO: Download blobs as .tar.gz in output folder 
+
+        # Return/Exit 0
+        exit 0
+      fi
+    done
+
+    # Exit with error if tag is incorrect after loop
+    echo "Error: Incorrect Tag"
+    exit 1
+  fi
+done
+
+# Exit with error if repo is incorrect after loop
+echo "Error: Incorrect repository"
+exit 1
